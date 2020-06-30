94121c0c37a9cee66ae37f07cbf5fd97ec05ad21
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2020-06-30 16:19:48 +0530

    header button

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ function App() {
         setText(e.target.value);
     }
 
+    function header(e) {
+        e.preventDefault();
+        setText(text + "### ");
+    }
+
     return (
         <div className="App">
             <h1>Markdown Previewer</h1>
@@ -14,6 +19,17 @@ function App() {
             <div className="container-textarea">
                 <div className="input-textarea-container">
                     <h3>Markdown text</h3>
+                    <div className="component-panel">
+                        <button onClick={header}>Header</button>
+                        <button>Bold</button>
+                        <button>Italic</button>
+                        <button>insert a quote</button>
+                        <button>insert code</button>
+                        <button>insert link</button>
+                        <button>Add a bulleted list</button>
+                        <button>Add a numbered list</button>
+                        <button>Add a task list</button>
+                    </div>
                     <textarea
                         cols="30"
                         rows="10"
