2cb00b0a1d0b05e64b5bd9d7c5bf6a1625fbe24e
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2020-06-30 15:25:55 +0530

    textarea and output

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 function App() {
-    return <div className="App">Hello, World!</div>;
+    const [text, setText] = useState();
+    function handleChange(e) {
+        setText(e.target.value);
+    }
+    return (
+        <div className="App">
+            <h1>Markdown Previewer</h1>
+
+            <div className="container-textarea">
+                <div className="input-textarea-container">
+                    <h3>Markdown text</h3>
+                    <textarea
+                        cols="30"
+                        rows="10"
+                        className="input-textarea"
+                        placeholder="Insert text"
+                        onChange={handleChange}
+                        value={text}
+                    ></textarea>
+                </div>
+                <div className="preview">
+                    <h3>Preview</h3>
+                    <p>{text}</p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
